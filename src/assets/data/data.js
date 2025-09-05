85d4e4c9c76407f84bee681d13a7b71387dc0fc5
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-09-05 08:52:41 +0200

    added tailwind,images and datafile

---
+++ src/assets/data/data.js
@@ -0,0 +1,51 @@
+import fcb from '../images/fcb.avif'
+import tot from '../images/tot.avif'
+import cfc from '../images/cfc.avif'
+import cfca from '../images/cfca.avif'
+import inter from '../images/inter.avif'
+import atm from '../images/atm.avif'
+
+export const items = [
+    {
+        id: 1,
+        image: fcb,
+        name: "F.C. Barcelona 2025/26",
+        type: "Home",
+        price: 1500
+    },
+    {
+        id: 2,
+        image: tot,
+        name: "Tottenham Hotspur 2025/26",
+        type: "Home",
+        price: 2200
+    },
+    {
+        id: 3,
+        image: cfc,
+        name: "Chelsea F.C. 2025/26",
+        type: "Home",
+        price: 1500
+    },
+    {
+        id: 4,
+        image: cfca,
+        name: "Chelsea F.C. 2025/26",
+        type: "Away",
+        price: 1500
+    },
+    {
+        id: 5,
+        image: inter,
+        name: "Inter Milan 2025/26",
+        type: "Third",
+        price: 1400
+    },
+    {
+        id: 6,
+        image: atm,
+        name: "Atlético Madrid 2025/26",
+        type: "Third",
+        price: 2100
+    }
+] 
