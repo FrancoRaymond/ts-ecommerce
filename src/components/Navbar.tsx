2d75e6fe1eaebc0d935362ee7c55ee09ef77e997
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-09-06 15:28:08 +0200

    added navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import cartIcon from '../assets/images/icon-cart.svg'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center px-3 py-3 bg-gray-50 shadow sm:px-5 md:px-10 lg:px-24'>
+        <h1 className='text-gray-800 text-2xl'>store</h1>
+        <button className='relative cursor-pointer active:scale-105'>
+            <img src={cartIcon} alt="" className='size-6'/>
+            <span className='bg-red-700 text-white text-xs p-0.5 absolute -top-2 -right-1 rounded-3xl'>3</span>
+        </button>
+    </div>
+  )
+}
+
+export default Navbar
