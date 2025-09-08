3bed25faac7772627cc4852687be09421f8a2107
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-09-08 18:20:19 +0200

    added cart component

---
+++ src/components/Cart.tsx
@@ -0,0 +1,11 @@
+const Cart = () => {
+  return (
+    <div className="cart fixed flex top-0 left-0 z-50 w-full min-h-screen px-2 sm:px-5 md:px-10 lg:px-24">
+        <div className='max-w-sm h-fit max-h-[500px] overflow-y-auto bg-white w-full p-5 ml-auto mt-14 rounded-sm flex flex-col'>
+        
+        </div>
+    </div>
+  )
+}
+
+export default Cart
